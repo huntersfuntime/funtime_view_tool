f5092cbd09049d035741200a4d5fa1e91c284d61
huntersfuntime <hunterorges@gmail.com>
2017-03-08 11:38:05 -0700

    Dev Tool Initial Commit

---
+++ lib/funtime_view_tool.rb
@@ -0,0 +1,5 @@
+require "funtime_view_tool/version"
+require "funtime_view_tool/renderer"
+module FuntimeViewTool
+  # Your code goes here...
+end
---
+++ lib/funtime_view_tool/version.rb
@@ -0,0 +1,3 @@
+module FuntimeViewTool
+  VERSION = "0.1.0"
+end
